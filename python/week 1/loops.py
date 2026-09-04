ba64bdf957d9463d525eb8899070633e5c7d4994
siri <jhashnavisiri@gmail.com>
2026-09-04 18:10:09 +0530

    update loops and percentage progaram

---
+++ python/week 1/loops.py
@@ -0,0 +1,42 @@
+# for i in range(2,40):
+#     print(i)
+#     if i%2==0:
+#         print(i)
+#         print("even")
+# else:
+#     print(i)
+#     print("odd")
+
+#     print ("in the loop")
+
+# print("end the loop")
+
+
+# for i in range (1,50,2):
+#     print(i)
+
+# for i in range(20,1,-1):
+#     print(i)
+
+# i=0
+# while i<5:
+#     print(i)
+#     i=i+1
+
+while True:
+    a=int(input("enter a number"))
+    if a%2==0:
+        print("even")
+    else:
+        print("odd")
+        
+
+
+
+    
+
+
+
+
+
+
---
+++ python/week 1/percentage.py
@@ -0,0 +1,13 @@
+a = float(input("enter your percentage"))
+if  a>100:
+    print("invalid percentage")
+elif a>80 :
+    print ("a")
+elif a>70  and a<80:
+    print("b")
+elif a>50 and a<70:
+    print("c")
+elif a>30 and a<50:
+    print ("f")
+     
+
