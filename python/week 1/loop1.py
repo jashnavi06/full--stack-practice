7f1b5dd039b8cef36f90b06a2e48a315f690950e
siri <jhashnavisiri@gmail.com>
2026-09-09 23:19:23 +0530

    added new files

---
+++ python/week 1/loop1.py
@@ -0,0 +1,13 @@
+n= int(input("enter a number"))
+for i in range(1 ,n):
+    print(i)
+    if i%2==0:
+        print(i)
+        print("even")
+    else:
+        print(i)
+        print("odd")
+    for i in range (1,50,2):
+              print(i) 
+
+
---
+++ python/week 1/loop3.py
@@ -0,0 +1,7 @@
+count = 0
+for i in range (1,60):
+    if i%2==0:
+        count+=1
+    else:
+        pass
+        print (count)
